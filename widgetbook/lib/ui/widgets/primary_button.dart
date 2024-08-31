473e58f3ab7b571baadd008875d4a1b107435401
Jens Horstmann <jens@widgetbook.io>
2024-08-31 18:17:41 +0200

    fix: remove PrimaryButton use-case

--- widgetbook/lib/ui/widgets/primary_button.dart
+++ widgetbook/lib/ui/widgets/primary_button.dart
@@ -6,18 +6,4 @@ import 'package:groceries_app/ui/ui.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
-@UseCase(
-  name: 'Default',
-  type: PrimaryButton,
-  designLink:
-      'https://www.figma.com/design/HsANkdhbsCNTkXBzNJRNLD/Groceries-Demo?node-id=7235-4663&t=N6qwmLP7MP59ClWB-4',
-)
-Widget buildPrimaryButtonCase(BuildContext context) {
-  return PrimaryButton(
-    content: context.knobs.string(label: 'content', initialValue: 'Text'),
-    trailing: FaIcon(
-      FontAwesomeIcons.arrowRight,
-      color: AppTheme.of(context).text.inverse,
-    ),
-  );
-}
+/// Add Widgetbook use-case here
