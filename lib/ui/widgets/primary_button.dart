46274c5ad0cabd21af95404cbdc6f95561c54083
Jens Horstmann <jens@widgetbook.io>
2024-09-02 10:21:33 +0200

    fead: add VoidCallback to PrimaryButton

--- lib/ui/widgets/primary_button.dart
+++ lib/ui/widgets/primary_button.dart
@@ -8,53 +8,58 @@ class PrimaryButton extends StatelessWidget {
     required this.content,
     this.leading,
     this.trailing,
+    this.onPressed,
   });
 
   final String content;
   final Widget? leading;
   final Widget? trailing;
+  final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        vertical: AppTheme.of(context).spacing.sm,
-        horizontal: AppTheme.of(context).spacing.l,
-      ),
-      decoration: BoxDecoration(
-        color: AppTheme.of(context).background.brand,
-        borderRadius: BorderRadius.circular(
-          AppTheme.of(context).radius.full,
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: AppTheme.of(context).spacing.sm,
+          horizontal: AppTheme.of(context).spacing.l,
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (leading != null)
-            Row(
-              children: [
-                leading!,
-                SizedBox(
-                  width: AppTheme.of(context).spacing.xs,
-                ),
-              ],
-            ),
-          Text(
-            content,
-            style: AppTheme.of(context).typography.label.copyWith(
-                  color: AppTheme.of(context).text.inverse,
-                ),
+        decoration: BoxDecoration(
+          color: AppTheme.of(context).background.brand,
+          borderRadius: BorderRadius.circular(
+            AppTheme.of(context).radius.full,
           ),
-          if (trailing != null)
-            Row(
-              children: [
-                SizedBox(
-                  width: AppTheme.of(context).spacing.xs,
-                ),
-                trailing!,
-              ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (leading != null)
+              Row(
+                children: [
+                  leading!,
+                  SizedBox(
+                    width: AppTheme.of(context).spacing.xs,
+                  ),
+                ],
+              ),
+            Text(
+              content,
+              style: AppTheme.of(context).typography.label.copyWith(
+                    color: AppTheme.of(context).text.inverse,
+                  ),
             ),
-        ],
+            if (trailing != null)
+              Row(
+                children: [
+                  SizedBox(
+                    width: AppTheme.of(context).spacing.xs,
+                  ),
+                  trailing!,
+                ],
+              ),
+          ],
+        ),
       ),
     );
   }
